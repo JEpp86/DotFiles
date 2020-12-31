27c27138b29aca82f475270ac7789a92f8329f79
JEpp86 <jasonepp0@gmail.com>
2020-12-31 12:26:08 -0800

    Fixed up the files to run on latest linux install

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 echo 'installing dependancies'
-sudo apt-get update
-sudo apt-get install build-essentials curl vim 
-echo 'installing apt-vim'
-curl -sL https://raw.githubusercontent.com/egalpin/apt-vim/master/install.sh | sh
+sudo apt-get update -y
+sudo apt-get install build-essential curl vim python-is-python3 -y
+echo 'installing pathogen'
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+echo 'installing NERDTree'
+git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree
 echo 'copying settings files and packages'
 cp bashrc ~/.bashrc
 cp profile ~/.profile
--- vimrc
+++ vimrc
@@ -1,8 +1,10 @@
+call pathogen#infect()
+syntax on
+filetype plugin indent on
 set autoindent
 set tabstop=4
 set ruler
 colorscheme default
-execute pathogen#infect()
 call pathogen#helptags()
 map <C-n> :NERDTreeToggle<CR>
 map <C-m> :tabp<CR>
